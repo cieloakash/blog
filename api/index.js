c3ab87a22a6c3890a4f06d3c89f25f7c2b1f581e
cieloakash <akpakash490@gmail.com>
2024-02-11 19:58:28 +0530

    database connect

--- api/index.js
+++ api/index.js
@@ -1,15 +1,31 @@
 // const express = require( 'express' );
 // var bodyParser = require('body-parser');
 
-import express from "express"
+import dotenv from "dotenv";
+import express from "express";
 import path from "path";
-import { fileURLToPath } from 'url';
+import { fileURLToPath } from "url";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+dotenv.config();
 
-const app = express()
-app.use(express.static(path.join(__dirname, "../Client/dist")))
+import mongoose from "mongoose";
+mongoose
+  .connect(
+    process.env.MONGOODB_CONNECTION_STRING
+  )
+  .then(() => {
+    console.log("MongoDB connected");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
-app.listen(3000,()=>{
-    console.log("this is node server");
-})
+console.log;
+
+const app = express();
+app.use(express.static(path.join(__dirname, "../Client/dist")));
+
+app.listen(3000, () => {
+  console.log("this is node server");
+});
