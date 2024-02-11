acc7fd05abf295e8d5a244f27ccc905510160515
cieloakash <akpakash490@gmail.com>
2024-02-11 19:05:18 +0530

    this is

--- api/index.js
+++ api/index.js
@@ -2,8 +2,13 @@
 // var bodyParser = require('body-parser');
 
 import express from "express"
+import path from "path";
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express()
+app.use(express.static(path.join(__dirname, "../Client/dist")))
 
 app.listen(3000,()=>{
     console.log("this is node server");
